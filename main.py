a088ce75e02126883639b4ad601dd4cb19b2ef76
RomanGoEmpire <roman.gerloff@proton.me>
2024-06-06 07:59:19 +0200

    add script to create md for photos

--- main.py
+++ main.py
@@ -1,75 +1,24 @@
 import os
+import re
 import logging
 
-PATH = "Test_Vault"
-DATA_PATH = f"{PATH}/Data"
+os.chdir(os.path.expanduser("~/Documents/Central"))
+PHOTOS_PATH = "Data/Photos"
+files = os.listdir(PHOTOS_PATH)
+base_files = [file for file in os.scandir(".") if file.is_file()]
 
-IMAGE_PATH = f"{DATA_PATH}/Images"
-VIDEO_PATH = f"{DATA_PATH}/Videos"
+for file in files:
+    base_files = [file for file in os.listdir(".") if file.is_file()]
+    year = file[:4]
+    year_md_file = f"Photos {year}.md"
+    if year_md_file not in base_files:
+        with open(year_md_file, "w") as f:
+            f.write(
+                """**Links:** [[Photos Album]]
+                    
+                    """
+            )
 
-
-NEW_DATA_PATH = DATA_PATH + "/{folder}"
-IMAGE_FORMATS = ["jpeg", "jpg", "png", "gif", "tiff", "bmp", "eps", "raw", "svg"]
-VIDEO_FORMATS = [
-    "mp4",
-    "avi",
-    "mov",
-    "wmv",
-    "flv",
-    "mkv",
-    "webm",
-    "vob",
-    "mts",
-    "m4v",
-    "3gp",
-    "f4v",
-]
-
-logging.basicConfig(level=logging.DEBUG)
-
-
-def get_files(path: str) -> list[str]:
-    return [f for f in os.scandir(path) if f.is_file()]
-
-
-def get_directories(path: str) -> list[str]:
-    return [f.name for f in os.scandir(path) if f.is_dir()]
-
-
-def create_folder(name: str) -> None:
-    logging.info(f"Created Folder: {name}")
-    os.makedirs(name=name, exist_ok=True)
-
-
-def move_file(file: str, new_folder: str) -> None:
-    logging.debug(f"Moved file to {new_folder}")
-    os.rename(file, f"{new_folder}/{file.name}")
-
-
-def main():
-    create_folder(DATA_PATH)
-    create_folder(IMAGE_PATH)
-
-    files = get_files(PATH)
-    directories = get_directories(DATA_PATH)
-
-    for file in files:
-        name, ending = file.name.split(".")
-
-        if ending == ".md":
-            continue
-
-        if ending in IMAGE_FORMATS:
-            new_folder = IMAGE_PATH
-        elif ending in VIDEO_FORMATS:
-            new_folder = VIDEO_PATH
-        else:
-            new_folder = NEW_DATA_PATH.format(folder=ending.upper())
-            if new_folder not in directories:
-                create_folder(new_folder)
-
-        move_file(file, new_folder)
-
-
-if __name__ == "__main__":
-    main()
+    with open(year_md_file, "a") as f:
+        f.write(f"[[{PHOTOS_PATH}/{file}]]")
+    break
