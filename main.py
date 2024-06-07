42d80b3da83fc978b226042e13c6536ccce4b39b
RomanGoEmpire <Roman.Gerloff@msg.group>
2024-06-07 19:00:35 +0200

    add file creation

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import re
 import logging
 
 os.chdir(os.path.expanduser("~/Documents/Central"))
-PHOTOS_PATH = "Data/Photos"
-files = os.listdir(PHOTOS_PATH)
-base_files = [file for file in os.scandir(".") if file.is_file()]
+PHOTOS_PATH = "Data/Videos"
+files = sorted(os.listdir(PHOTOS_PATH))
 
-for file in files:
-    base_files = [file for file in os.listdir(".") if file.is_file()]
+for index, file in enumerate(files):
+    base_files = [file.name for file in os.scandir(".") if file.is_file()]
     year = file[:4]
     year_md_file = f"Photos {year}.md"
     if year_md_file not in base_files:
         with open(year_md_file, "w") as f:
             f.write(
                 """**Links:** [[Photos Album]]
-                    """
+
+"""
             )
 
     with open(year_md_file, "a") as f:
-        f.write(f"[[{PHOTOS_PATH}/{file}]]")
-    break
+        f.write(
+            f"""[[{file}]]
+"""
+        )
